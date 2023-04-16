46681353a9a95bb4ea8419c7e8a02da79fb8fc76
Sandy Tanwisuth <11690577+sandguine@users.noreply.github.com>
2023-04-16 02:58:52 -0700

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -2,15 +2,19 @@ import os
 import openai
 from dotenv import load_dotenv; load_dotenv()
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def generate_response(prompt):
-  response = openai.Completion.create(
-    engine="davinci",
-    prompt=prompt,
-    max_tokens=1024,
-    n=1,
-    stop=None,
-    temperature=0.5,
-  )
-  return response.choices[0].text.strip()
+def chat_gpt_request(emotion, prompt, max_tokens=100, n=1, stop=None, temperature=1.0):
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    
+    full_prompt = f"{emotion}: {prompt}"
+
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=full_prompt,
+        max_tokens=max_tokens,
+        n=n,
+        stop=stop,
+        temperature=temperature,
+    )
+
+    return response.choices[0].text.strip()
