fb680fd6061aa874bb0a6b06faa0452eb8c2159c
Sandy Tanwisuth <11690577+sandguine@users.noreply.github.com>
2023-04-16 03:06:10 -0700

    Remove Get method and HTML Template. Need to adjust face API accordingly to correct inputs.

--- app.py
+++ app.py
@@ -101,39 +101,22 @@
 #     app.run(port=4444)
 
 from flask import Flask, request, render_template_string
-from gpt import generate_response
+from gpt import chat_gpt_request
+
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == "POST":
-        prompt = request.form["prompt"]
-        image_url = request.form["image_url"]
-        emotion = detect_emotion(image_url, subscription_key)
-        response = generate_response(prompt)
-        response_with_emotion = f"{emotion.capitalize()} {response}"
-        return render_template_string(html_template, response=response_with_emotion)
-    else:
-        return render_template_string(html_template)
-
-html_template = """
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>ChatGPT</title>
-  </head>
-  <body>
-    <h1>ChatGPT</h1>
-    <form method="POST">
-      <label for="prompt">Enter a prompt:</label>
-      <input type="text" id="prompt" name="prompt" required>
-      <label for="image_url">Enter an image URL:</label>
-      <input type="text" id="image_url" name="image_url" required>
-      <button type="submit">Generate response</button>
-    </form>
-    {% if response %}
-      <p>{{ response }}</p>
-    {% endif %}
-  </body>
-</html>
-"""
+@app.route('/process_input', methods=['POST'])
+def process_input():
+    data = request.get_json()
+    
+    image_url = data.get('image_url')
+    user_text = data.get('user_text')
+    
+    face_api_key = "your-face-api-key"
+    face_api_endpoint = "your-face-api-endpoint"
+    
+    emotion = detect_emotion(image_url, face_api_key, face_api_endpoint)
+    
+    chatgpt_response = chat_gpt_request(emotion, user_text)
+    
+    return jsonify({"response": chatgpt_response})
